78788c6c1dcfea532575b29a26869c41b391a810
paldeflex <paldeflex@gmail.com>
2026-05-11 18:38:27 +0500

    Заготовка контроллеров и роутов аутентификации

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+
+    public function showLoginForm()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function showRegistrationForm()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HealthCheckController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HealthCheckController::class, '__invoke']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::redirect('/', '/dashboard');
+});
+
+Route::get('/health-check', [HealthCheckController::class, '__invoke']);
 Route::get('/health-check/run', [HealthCheckController::class, 'runCheck']);
