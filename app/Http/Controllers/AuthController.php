065b0d5ae09938575564a81a0c6bf2ef6e4b19eb
paldeflex <paldeflex@gmail.com>
2026-05-11 21:04:27 +0500

    Заготовки blade шаблонов аутентификации

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
+
 class AuthController extends Controller
 {
 
+    /**
+     * @throws BindingResolutionException
+     */
     public function showLoginForm()
     {
-
+        return view('components.auth.login');
     }
 
     public function login()
@@ -15,9 +20,12 @@ class AuthController extends Controller
 
     }
 
+    /**
+     * @throws BindingResolutionException
+     */
     public function showRegistrationForm()
     {
-
+        return view('components.auth.register');
     }
 
     public function register()
